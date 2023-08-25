a8769e41be1f01e8585e807a15ed08b792932099
Laryssa Ferreira <laryssa.oliveira1902@gmail.com>
2023-08-25 15:47:12 -0300

    pós reunião nicholas - build e api key

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cp index.html public/index.html
+cp -r assets/ public/assets
+
+# Lê a chave de API do arquivo apikey.txt
+api_key=$(cat apikey.txt)
+
+# Substitui a expressão 'xyz' pela chave de API no arquivo public/index.html
+sed -i '' "s/your api key here/$api_key/g" public/index.html # macos = string vazia 
+
+echo "Substituição concluída."
